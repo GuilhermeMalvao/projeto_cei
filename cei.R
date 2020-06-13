38956f03c0da149e2c6733354224441d293037a6
gdmalvao <gdmalvao@hotmail.com>
2020-06-13 16:10:43 -0300

    analise exploratoria dos dados

--- cei.R
+++ cei.R
@@ -3,10 +3,21 @@ library(lubridate)
 library(tidyr)
 library(dplyr)
 library(stringr)
-dados <- read.xlsx("cei.xlsx") %>% 
-  select(-c(3,4,6)) %>% 
-  mutate(Data.do.Negócio = as_date(Data.do.Negócio,origin = "1899-12-30 00:00:00")) %>% 
-  mutate(ano_mes = str_sub(Data.do.Negócio,1,7))
+library(janitor)
+
+dados <- read.xlsx("cei.xlsx") %>%
+  clean_names() %>% 
+  select(-c(3,4,6,10)) %>% 
+  mutate(data_do_negocio = as_date(data_do_negocio,origin = "1899-12-30 00:00:00")) %>% 
+  mutate(ano_mes = str_sub(data_do_negocio,1,7)) 
+
+dados %>% glimpse()
+
+dados1 <- dados %>%
+  filter(codigo_negociacao == "B3SA3") %>% 
+  group_by(ano_mes,compra_venda,codigo_negociacao) %>% 
+  
+  summarise(pm = (quantidade*preco_r)/sum(quantidade))
 
 dados1 <- dados %>%
   mutate(Quantidade = if_else(`Compra/Venda` == "V",-Quantidade,Quantidade))
