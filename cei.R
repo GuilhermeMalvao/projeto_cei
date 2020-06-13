d70636c0bd6c80f4c64728190d65c1e8ff9d1247
gdmalvao <gdmalvao@hotmail.com>
2020-06-13 17:08:56 -0300

    calculos no excel para colocar em codigo no R, até sexta que vem, não esquecer

--- cei.R
+++ cei.R
@@ -14,12 +14,22 @@ dados <- read.xlsx("cei.xlsx") %>%
 dados %>% glimpse()
 
 dados1 <- dados %>%
-  filter(codigo_negociacao == "B3SA3") %>% 
-  group_by(ano_mes,compra_venda,codigo_negociacao) %>% 
-  
-  summarise(pm = (quantidade*preco_r)/sum(quantidade))
+  filter(codigo_negociacao == "B3SA3F") %>% 
+  group_by(ano_mes, compra_venda, codigo_negociacao) %>% 
+  summarise(pm = sum(quantidade*preco_r)/sum(quantidade),
+            qtd = sum(quantidade), 
+            v_total = sum(valor_total_r)) %>%  
+  ungroup() %>% 
+  mutate(aux = if_else(compra_venda == "C",1,-1),
+         qtd_acumulada = cumsum(qtd*aux),
+         
+         v_total1 = cumsum(lag(qtd_acumulada,default = 0)*pm*aux))
 
-dados1 <- dados %>%
-  mutate(Quantidade = if_else(`Compra/Venda` == "V",-Quantidade,Quantidade))
+write.xlsx(dados1,"d.xlsx")
 
+
+
+(11*32.53+30*39.87133)/41
+43.1833-37.9017
+(43.1833-37.9017)*30
 
